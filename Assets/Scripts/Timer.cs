18014f0fa2a5c47451427a807b1a39c44bc2c481
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-11-15 13:00:42 +0000

    Updated timer
    Better UI, will need a fix to stop it from displaying "60" whenever it goes over a minute though.
    
    Also added score text, code TODO.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 #region 'Using' information
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,7 +9,7 @@ using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    float countdown = 180.0f; // Counts down for 3 minutes.
+    float countdown = 183.0f; // Counts down for 3 minutes and 3 seconds.
     public Text timerUI;
 
     void Update()
@@ -16,13 +17,13 @@ public class Timer : MonoBehaviour
         if (countdown > 0)
         { countdown -= Time.deltaTime; }
 
+        string minutes = ((int)countdown / 60).ToString();
+        string seconds = (countdown % 60).ToString("00");
 
         double b = System.Math.Round(countdown, 2);
-        timerUI.text = b.ToString();
-        if (countdown < 0)
-        {
-            Debug.Log("Completed");
-            SceneManager.LoadScene(sceneBuildIndex: 0);
-        }
+        timerUI.text = minutes + ":" + seconds;
+
+        if (countdown < 0) // Sends you back to the main menu when the timer hits 0.
+        { SceneManager.LoadScene(sceneBuildIndex: 0); }
     }
 }
