70c561d310e131048f64f42392da78940ece22df
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-11-22 12:12:15 +0000

    Main menu completed
    All SFX credited.

--- Assets/Scripts/MainMenu/OpenLink.cs
+++ Assets/Scripts/MainMenu/OpenLink.cs
@@ -27,4 +27,19 @@ public class OpenLink : MonoBehaviour
 
     public void FontCredit()
     { Application.OpenURL("https://www.1001fonts.com/ticketing-font.html#license"); }
+
+    public void DogBarkingCredit()
+    { Application.OpenURL("https://soundbible.com/2215-Labrador-Barking-Dog.html"); }
+
+    public void GlassBreakingCredit()
+    { Application.OpenURL("https://soundbible.com/1761-Glass-Breaking.html"); }
+
+    public void BedspringSqueakCredit()
+    { Application.OpenURL("https://soundbible.com/946-Bedspring-Squeak.html"); }
+
+    public void AngryCatCredit()
+    { Application.OpenURL("https://soundbible.com/1363-Angry-Cat.html"); }
+
+    public void SlapCredit()
+    { Application.OpenURL("https://mixkit.co/free-sound-effects/hit/?page=2"); }
 }
