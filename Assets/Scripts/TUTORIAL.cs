656808a4bd50e5fa980b710f9d66570d07c89559
SquidGeneral10 <SquidGeneral10GameDev@Gmail.com>
2021-12-01 16:47:20 +0000

    Level design updated
    Still need to fix platforms, add tutorial, and make further changes later.

---
+++ Assets/Scripts/TUTORIAL.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TUTORIAL : MonoBehaviour
+{
+    public void HideTutorial()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) // Checks for any keyboard input. 
+        {
+            
+        }
+    }
+}
